c504528e98711a740d9f50a8a35317e0d3974552
Md. Jahidul Islam <jahid.jabed@gmail.com>
2022-06-20 22:59:17 +0600

    Add required files

---
+++ load.js
@@ -0,0 +1,53 @@
+/* When the user scrolls down 20px from the top of the document, slide down the navbar */
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+	document.getElementById("navbar").style.top = "0px";
+  } else {
+	document.getElementById("navbar").style.top = "0px";
+  }
+}
+
+/* Social Tags Loader */
+var socialtags = [{
+    img: "img/github_icon.svg",
+    link: "https://github.com/DataParadox"
+}, {
+    img: "img/linkedin_icon.svg",
+    link: "#"
+}]
+
+function loadSocialIcons() {
+    var socialtext = ""
+    for (x in socialtags) {
+        var data = `<a class="center" href="` + socialtags[x].link + `" target=_blank> <img class="center" style="padding: 5px!important;width: 40px;" src="` + socialtags[x].img + `"></a>`
+        socialtext += data
+    }
+    document.getElementById("social-list").innerHTML = socialtext;
+}
+
+loadSocialIcons()
+
+/* Day Night Toggler */
+function dayNightToggle() {
+    var element = document.body;
+    element.classList.toggle("light-mode");
+    var check = window.localStorage.getItem('light-mode');
+    if (check == "light") {
+        window.localStorage.clear();
+    } else {
+        window.localStorage.setItem('light-mode', 'light');
+    }
+
+}
+
+function checkDayNight() {
+    var check = window.localStorage.getItem('light-mode');
+    var element = document.body;
+    if (check == "light") {
+        element.classList.add("light-mode");
+    } else {
+        element.classList.remove("light-mode");
+    }
+}
