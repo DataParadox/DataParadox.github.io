7d994f499fb7531682c19159c2934e8310bf7193
Md. Jahidul Islam <jahid.jabed@gmail.com>
2022-06-26 21:05:22 +0600

    Update load.js

--- js/load.js
+++ js/load.js
@@ -39,7 +39,7 @@ function checkDayNight() {
     }
 }
 
-window.onload = checkDayNight();
+window.onload = function() {checkDayNight()};
 
 /* When the user scrolls down 20px from the top of the document, slide down the navbar */
 window.onscroll = function() {scrollFunction()};
